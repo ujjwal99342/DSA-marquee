6ac618fb6f79b6366d75f61eb191c1dcdfde604d
ujjwal kumar singh <2403051240097@paruluniversity.ac.in>
2026-06-05 08:57:06 +0530

    Add files via upload

---
+++ functions/celciustofar.java
@@ -0,0 +1,11 @@
+class celciustofar{
+    public static double celciustofar(int celcius){
+        double far = (1.8*celcius)+32;
+        System.out.println(far);
+        return far;
+    }
+    public static void main(String[] args){
+        celciustofar(90);
+    }
+
+}
